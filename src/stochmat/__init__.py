5b744f215182b19dd878a27c6ff25adaee5d0c56
Jonas I. Liechti <j-i-l@t4d.ch>
2026-04-23 16:37:24 +0200

    propagate version into build

--- src/stochmat/__init__.py
+++ src/stochmat/__init__.py
@@ -1,3 +1,10 @@
+try:
+    # try to import version (provided by hatch (see pyproject.toml)
+    from ._version import __version__
+except ImportError:
+    # Fallback if the package wasn't installed properly
+    __version__ = "unknown"
+
 from .sparse_stoch_mat import (
     inplace_csr_matmul_diag,
     inplace_csr_row_normalize,
